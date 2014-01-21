191bf57332032f83faf5a93ca83822ef2056394b
nikitinsm <nikitinsm@gmail.com>
2014-01-21 14:55:48 +0400

    ADD: Social Event index + tests

--- src/indexer/base/block.py
+++ src/indexer/base/block.py
@@ -3,26 +3,26 @@ from collections import OrderedDict
 """
 #http://docs.python.org/2/library/struct.html
 
-Format	    C Type	                Python type	        Standard size
+Format      C Type                  Python type         Standard size
 ---------------------------------------------------------------------
-x	        pad byte	            no value
-c	        char	                string of length    1
-b	        signed char	            integer	            1
-B	        unsigned char           integer	            1
-?	        _Bool	                bool	            1
-h	        short	                integer	            2
-H	        unsigned short	        integer	            2
-i	        int	                    integer	            4
-I	        unsigned int	        integer	            4
-l	        long	                integer	            4
-L	        unsigned long	        integer	            4
-q	        long long	            integer	            8
-Q	        unsigned long long	    integer	            8
-f	        float	                float	            4
-d	        double	                float	            8
-s	        char[]	                string
-p	        char[]	                string
-P	        void *	                integer
+x           pad byte                no value
+c           char                    string of length    1
+b           signed char             integer             1
+B           unsigned char           integer             1
+?           _Bool                   bool                1
+h           short                   nteger              2
+H           unsigned short          integer             2
+i           int                     integer             4
+I           unsigned int            integer             4
+l           long                    integer             4
+L           unsigned long           integer             4
+q           long long               integer             8
+Q           unsigned long long      integer             8
+f           float                   float               4
+d           double                  float               8
+s           char[]                  string
+p           char[]                  string
+P           void *                  integer
 ---------------------------------------------------------------------
 """
 
